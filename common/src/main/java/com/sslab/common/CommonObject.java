02cd871198f6cc05bddfb3bc4fbf44f493c26ab5
Lauren Schroeder <exxodus7@gmail.com>
2015-08-10 16:59:14 -0700

    Created common object

--- common/src/main/java/com/sslab/common/CommonObject.java
+++ common/src/main/java/com/sslab/common/CommonObject.java
@@ -1,15 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.sslab.common;
 
+import org.springframework.data.anotation.Id;
+
 /**
- *
- * @author lauren
+ * Provides common functionlity for all wilson objects.
  */
 public class CommonObject {
+    @Id
+    private String id;
     
+    public CommonObject() {}
 }
